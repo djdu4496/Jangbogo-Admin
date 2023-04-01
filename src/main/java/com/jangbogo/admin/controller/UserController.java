24a4af4d3621c2817bc4232bc4d0105b9ea1d37e
jinvicky <jinvicky17@gmail.com>
2023-04-01 21:28:31 +0900

    브랜치 테스트

--- src/main/java/com/jangbogo/admin/controller/UserController.java
+++ src/main/java/com/jangbogo/admin/controller/UserController.java
@@ -16,21 +16,14 @@ import java.util.List;
 import java.util.Map;
 
 @Controller
-@RequestMapping("") //여기에 경로를 설정하지 말것. css, js에 이 경로가 추가되어서 제대로 적용 X.
-public class MemberController {
+public class UserController {
 
     //주입
     @Autowired
     MemberServiceImpl service;
 
-    //배운 ModelAttribute를 적용해 보았다.
-//    @ModelAttribute("userList")
-//    public List<User> storeUserList () throws Exception{
-//        return service.getUserList();
-//    }
-
     @GetMapping("/user/list")
-    public String adminMain(@RequestParam(defaultValue ="1") Integer page,
+    public String adminMain(@RequestParam(defaultValue = "1") Integer page,
                             @RequestParam(defaultValue = "10") Integer pageSize,
                             Model m, HttpServletRequest request) throws Exception{
 
