362838b3e12ac23010b5b695365e1ff9c8efb60a
djdu4496 <djdu4496@gmail.com>
2023-05-05 14:57:13 +0900

    fix : 상품신청 '반려' 요청 시, URL 수정

--- src/main/java/com/jangbogo/admin/controller/ProductController.java
+++ src/main/java/com/jangbogo/admin/controller/ProductController.java
@@ -58,7 +58,7 @@ public class ProductController {
         try {
             productDetailDto = productService.getProductRead(prod_idx);
             if(productDetailDto == null) throw new Exception("getProductRead failed");
-            model.addAttribute("product", productDetailDto);                                               // Model에 idx를 K/V로 저장
+            model.addAttribute("product", productDetailDto);                                                            // Model에 idx를 K/V로 저장
 
             return "/product/read";                                                                                      // "/order/order.jsp" 뷰 이름 반환 - 뷰 렌더링
         } catch(Exception e) {                                                                                          // 에러 발생 시
@@ -100,8 +100,8 @@ public class ProductController {
         try {
             productDetailDto = productService.getProductRead(prod_idx);
             if(productDetailDto == null) throw new Exception("getProductRead failed");
-            model.addAttribute("product", productDetailDto);                                               // Model에 '상품상세' 객체를 K/V로 저장
-            model.addAttribute("prod_idx", prod_idx);                                                      // Model에 '상품번호'를 K/V로 저장
+            model.addAttribute("product", productDetailDto);                                                            // Model에 '상품상세' 객체를 K/V로 저장
+            model.addAttribute("prod_idx", prod_idx);                                                                   // Model에 '상품번호'를 K/V로 저장
             return "/product/readPending";                                                                              // "/order/order.jsp" 뷰 이름 반환 - 뷰 렌더링
         } catch(Exception e) {                                                                                          // 에러 발생 시
             e.printStackTrace();                                                                                        // 1) 에러 내용을 로그에 출력
@@ -109,21 +109,21 @@ public class ProductController {
         }
     }
 
-    // 메서드명 : approveProductPending
-    // 기   능 : '승인대기(1)' 상태인 상품의 상태를 '승인완료(2)' 상태로 수정
+    // 메서드명 : updateProductPending
+    // 기   능 : '승인대기(1)' 상태인 상품의 상태를 '승인완료(2)' 또는 '승인반려(3)' 상태로 수정
     // 반환타입 : ResponseEntity<String>
     // 매개변수 : Map<Integer, Integer> pathVarsMap - Integer prod_idx (상품번호), reg_stae_cd (상품상태)
     // 요청URL : /product/approve/{prod_idx} PATCH
-    @PatchMapping("/product/approve/{prod_idx}/{reg_state_cd}")
-    public ResponseEntity<String> approveProductPending(@PathVariable Map<Integer, Integer> pathVarsMap) {
+    @PatchMapping("/product/update/{prod_idx}/{reg_state_cd}")
+    public ResponseEntity<String> updateProductPending(@PathVariable Map<Integer, Integer> pathVarsMap) {
         int rowCnt = 0;
         try {
             rowCnt = productService.updateProductRegState(pathVarsMap);                                                 // 상품번호 #{prod_idx}에 해당하는 상품의 상품상태코드를 #{reg_state_cd}로 변경 후 변경 있는 행의 수를 rowCnt에 저
             if(rowCnt == 0) throw new Exception("updateProductRegState failed(PROD_DTL)");                              // 변경된 행의 개수가 0인 경우 예외를 발생시킨다.
-            return new ResponseEntity<>("MOD_REG_STATE_CD_OK", HttpStatus.OK);                                    // update 요청 결과가 성공인 경우, 상태코드와 함께 메시지 반환
+            return new ResponseEntity<>("MOD_REG_STATE_CD_OK", HttpStatus.OK);                                          // update 요청 결과가 성공인 경우, 상태코드와 함께 메시지 반환
         } catch(Exception e) {
             e.printStackTrace();                                                                                        // 1) 에러 내용을 로그에 출력
-            return new ResponseEntity<>("MOD_REG_STATE_CD_ERR", HttpStatus.BAD_REQUEST);                          // 2) update 요청 결과가 실패인 경우, 상태코드와 함께 메시지 반환
+            return new ResponseEntity<>("MOD_REG_STATE_CD_ERR", HttpStatus.BAD_REQUEST);                                // 2) update 요청 결과가 실패인 경우, 상태코드와 함께 메시지 반환
         }
     }
 }
